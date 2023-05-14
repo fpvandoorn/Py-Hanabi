258b804576e1babe59100537e687b1c1929c272d
Maximilian Keßler <git@maximilian-kessler.de>
2023-05-14 14:04:57 +0200

    add alive_bar to requirements

--- site_api.py
+++ site_api.py
@@ -1,31 +1,36 @@
 import json
 import requests
 import requests_cache
+from log_setup import logger
 
 
 # Cache all requests to site to reduce traffic and latency
 session = requests_cache.CachedSession('hanab.live')
 
 
-def get(url):
+def get(url, refresh=False):
     #    print("sending request for " + url)
     query = "https://hanab.live/" + url
-    response = session.get(query)
+    logger.debug("GET {}".format(query))
+    response = session.get(query, force_refresh=refresh)
     if not response:
-        raise RuntimeError("Failed to get request {} from hanab.live".format(query))
+        logger.error("Failed to get request {} from hanab.live".format(query))
+        return None
     if not response.status_code == 200:
         return None
     if "application/json" in response.headers['content-type']:
         return json.loads(response.text)
 
-def api(url):
+
+def api(url, refresh=False):
     link = "api/v1/" + url
     if "?" in url:
         link += "&"
     else:
         link += "?"
     link += "size=100"
-    return get(link)
+    return get(link, refresh)
+
 
 def replay(seed):
     r = api("seed/" + str(seed))
